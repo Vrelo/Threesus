a3449f7b8a98876fcd8344adf04ff2c243818388
Vrelo <pakp122134@gmail.com>
2016-12-11 18:04:13 -0500

    Our AI File.
    start of the end

---
+++ ThreesusCore/Source/Bots/OurBot.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Threesus.CoreGame;
+
+namespace Threesus.Bots
+{
+    public class OurBot : IBot
+    {
+        /// <summary>
+        /// Returns the next move to make based on the state of the specified game, or null to make no move.
+        /// </summary>
+        ShiftDirection? GetNextMove(FastBoard board, FastDeck deck, NextCardHint nextCardHint, ref long movesEvaluated) {
+            return null;
+        }
+
+        /// <summary>
+        /// Returns the next move to make based on the state of the specified game, or null to make no move.
+        /// </summary>
+        ShiftDirection? GetNextMove(FastBoard board, FastDeck deck, NextCardHint nextCardHint) {
+            long movesEvaluated = 0;
+            return GetNextMove(board, deck, nextCardHint, ref movesEvaluated);
+        }
+    }
+}
