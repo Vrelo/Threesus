be617788a0abce900bd64da6421abace5146bbce
Vrelo <pakp122134@gmail.com>
2016-12-11 18:29:22 -0500

    ourBot

--- ThreesusCore/Source/Bots/OurBot.cs
+++ ThreesusCore/Source/Bots/OurBot.cs
@@ -12,14 +12,14 @@ namespace Threesus.Bots
         /// <summary>
         /// Returns the next move to make based on the state of the specified game, or null to make no move.
         /// </summary>
-        ShiftDirection? GetNextMove(FastBoard board, FastDeck deck, NextCardHint nextCardHint, ref long movesEvaluated) {
+        public ShiftDirection? GetNextMove(FastBoard board, FastDeck deck, NextCardHint nextCardHint, ref long movesEvaluated) {
             return null;
         }
 
         /// <summary>
         /// Returns the next move to make based on the state of the specified game, or null to make no move.
         /// </summary>
-        ShiftDirection? GetNextMove(FastBoard board, FastDeck deck, NextCardHint nextCardHint) {
+        public ShiftDirection? GetNextMove(FastBoard board, FastDeck deck, NextCardHint nextCardHint) {
             long movesEvaluated = 0;
             return GetNextMove(board, deck, nextCardHint, ref movesEvaluated);
         }
